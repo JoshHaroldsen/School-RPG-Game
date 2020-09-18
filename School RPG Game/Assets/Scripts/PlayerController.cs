e1db49311e18c51f8bbc4dffbac411b0b607c436
Josh Haroldsen <Joshdharoldsen@gmail.com>
2020-09-18 09:58:51 -0600

    Edited the script so that the player can now move

--- School RPG Game/Assets/Scripts/PlayerController.cs
+++ School RPG Game/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    private float xInput;
+    private float yInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Moves player Left and Right
+        xInput = Input.GetAxis("Horizontal");
+
+        // Moves player Up and Down
+        yInput = Input.GetAxis("Vertical");
+
+        // Moves player up and down based on vertical input
+        transform.Translate(Vector2.up * Time.deltaTime * speed * yInput);
+
+        // Moves player left and right based on horizontal input
+        transform.Translate(Vector2.right * Time.deltaTime * speed * xInput);
+
+
     }
 }
